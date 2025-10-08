0e84b115a8de14bc7bcda0c8738d8ace3f7ebd27
rob <robert@rusk.ie>
2025-10-08 16:54:38 +0100

    extract default fifo path

--- client.py
+++ client.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 import sys
+from server import DEFAULT_FIFO_PATH
 
 message = sys.argv[1] if len(sys.argv) > 1 else 'hello'
-fifo_path = sys.argv[2] if len(sys.argv) > 2 else '/run/sky-remote/fifo'
+fifo_path = sys.argv[2] if len(sys.argv) > 2 else DEFAULT_FIFO_PATH
 
 with open(fifo_path, 'w') as f:
   f.write(f'{message}\n')
--- server.py
+++ server.py
@@ -3,14 +3,17 @@
 import sys
 from sky_remote import SkyRemote
 
+# Default configuration
+DEFAULT_FIFO_PATH = '/run/sky-remote/fifo'
+
 if len(sys.argv) < 2:
     print("Usage: server.py <sky_addr> [fifo_path]")
     print("  sky_addr: IP address of Sky Q box (required)")
-    print("  fifo_path: Path to FIFO (default: /run/sky-remote/fifo)")
+    print(f"  fifo_path: Path to FIFO (default: {DEFAULT_FIFO_PATH})")
     sys.exit(1)
 
 sky_addr = sys.argv[1]
-fifo_path = sys.argv[2] if len(sys.argv) > 2 else '/run/sky-remote/fifo'
+fifo_path = sys.argv[2] if len(sys.argv) > 2 else DEFAULT_FIFO_PATH
 
 r = SkyRemote(sky_addr)
 
@@ -27,8 +30,10 @@ def do_read(f):
         break
 
     try:
+        # sky-remote defines int keys for the numbers
         r.press(int(k))
     except:
+        # ah must be a non-int key
         r.press(k)
 
 
